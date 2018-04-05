d96d45bc5d10e489155e8430a8cd7d36e4acfa4b
Николай <it-nikola@mail.ru>
2018-04-05 18:16:23 +0300

    вторая часть задания урока 2

---
+++ basic/models/Product.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $price
+ * @property string $created_at
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'price'], 'required'],
+            [['created_at'], 'safe'],
+            [['name', 'price'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'price' => 'Price',
+            'created_at' => 'Created At',
+        ];
+    }
+}
