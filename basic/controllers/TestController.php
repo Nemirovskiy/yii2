4c99a89cd590079924f137745144447ef24d84f7
Николай <it-nikola@mail.ru>
2018-04-16 10:13:49 +0300

    Выполнено задание к уроку 4

--- basic/controllers/TestController.php
+++ basic/controllers/TestController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\components\TestService;
 use Yii;
+use yii\db\Query;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\Response;
@@ -25,8 +26,51 @@ class TestController extends Controller
 //                'Illum in provident tenetur voluptas!'
 //            ]
 //        ]);
+        return $this->renderContent('44');
+    }
+    public function actionInsert(){
+        \Yii::$app->db->createCommand("INSERT INTO user".
+              "(username,name,password_hash) VALUES ('first','Первый','77777'),('user','User','sssss')".
+            ",('login','Login','lllllll'),('last','Last','rrrrrr')")->execute();
 
-        return $this->render('index',['head'=> Yii::$app->test->showName()]);
+        $user = (new Query())->from('user')->select('id,username')->indexBy('username')->column();
+
+        \Yii::$app->db->createCommand()->batchInsert('note',['text','creator_id'],[
+            [
+                'Идейные соображения высшего порядка, а также новая модель организационной деятельности.',
+                $user['user']
+            ],
+            [
+                'Безусловно, сплоченность команды профессионалов однозначно определяет каждого участника.',
+                $user['login']
+            ],
+            [
+                'Сложно сказать, почему интерактивные прототипы своевременно верифицированы. С другой стороны.',
+                $user['last']
+            ],
+            [
+                'С другой стороны, дальнейшее развитие различных форм деятельности в значительной степени.',
+                $user['first']
+            ],
+            [
+                'Значимость этих проблем настолько очевидна, что сплоченность команды профессионалов однозначно.',
+                $user['user']
+            ]
+        ])->execute();
+        return $this->renderContent('44');
+    }
+
+    public function actionSelect(){
+        $result['first'] = (new Query())->from('user')->where('id = :id',[':id' => 1])->one();
+        $result['all'] = (new Query())->from('user')->where('id > :id',[':id' => 1])->orderBy('name')->all();
+        $result['count'] = (new Query())->from('user')->count();
+        $result['login'] = (new Query())->from('user')->select('id,username')->indexBy('username')->column();
+        /**
+         * SELECT note.id note_id, text, user.id creator_id, user.username creator_name
+         * FROM `note` INNER JOIN `user` ON creator_id = user.id
+         */
+        $result['note'] = (new Query())->from('note')->select('note.id note_id, text, user.id creator_id, user.username creator_name')->innerJoin('user','creator_id = user.id')->indexBy('note_id')->all();
+        _end($result);
     }
 }
 
